88313473709bc398997aa4222cc2a9b13904c802
priyanshu <priyanshugangwar17@gmail.com>
2026-05-21 17:05:09 +0530

    Add app context and responsive sidebar functionality

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,69 @@
+import { createContext, useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyChats, dummyUserData } from "../assets/assets"
+
+const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [chats, setChats] = useState([]);
+    const [selectedChats, setSelectedChats] = useState(null);
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+    const fetchUser = async () => {
+        setUser(dummyUserData)
+    }
+
+    const fetchUserChats = async () => {
+        setChats(dummyChats)
+        setSelectedChats(dummyChats[0])
+    }
+
+    useEffect(() => {
+        if (theme === 'dark') {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+        localStorage.setItem('theme', theme);
+    }, [theme])
+
+    useEffect(() => {
+        if (user) {
+            fetchUserChats()
+        } else {
+            setChats([])
+            setSelectedChats(null)
+        }
+    }, [user])
+
+
+    useEffect(() => {
+        fetchUser()
+    }, [])
+
+    const value = {
+        navigate,
+        user,
+        setUser,
+        chats,
+        setChats,
+        selectedChats,
+        setSelectedChats,
+        theme,
+        setTheme,
+        fetchUser
+    }
+
+    return (
+        <AppContext.Provider value={value} >
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useAppContext = () => {
+    return useContext(AppContext);
+}
